fd5e168b3f238adceb87e148736624da5a62f22e
wadamasaya <16010418@anabuki-college.ac.jp>
2018-06-07 09:39:26 +0900

    test1

--- app/src/main/java/wadamasaya/lifecyclesample/LifeCycleSubActivity.java
+++ app/src/main/java/wadamasaya/lifecyclesample/LifeCycleSubActivity.java
@@ -6,52 +6,51 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
-public class LifeCycleMainActivity extends AppCompatActivity {
+public class LifeCycleSubActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.i("LifeCycleSample","Main onCreate() called.");
+        Log.i("LifeCycleSample","Sub onCreate() called.");
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_life_cycle_main);
+        setContentView(R.layout.activity_life_cycle_sub);
     }
     @Override
     public void onStart(){
-        Log.i("LifeCycleSample","Main onStart() called.");
+        Log.i("LifeCycleSample","Sub onStart() called.");
         super.onStart();
     }
 
     @Override
     public void onRestart(){
-        Log.i("LifeCycleSample","Main onRestart() called.");
+        Log.i("LifeCycleSample","Sub onRestart() called.");
         super.onRestart();
     }
 
     @Override
     public void onResume(){
-        Log.i("LifeCycleSample","Main onResume() called.");
+        Log.i("LifeCycleSample","Sub onResume() called.");
         super.onResume();
     }
 
     @Override
     public void onPause(){
-        Log.i("LifeCycleSample","Main onPause() called.");
+        Log.i("LifeCycleSample","Sub onPause() called.");
         super.onPause();
     }
 
     @Override
     public void onStop(){
-        Log.i("LifeCycleSample","Main onStop() called.");
+        Log.i("LifeCycleSample","Sub onStop() called.");
         super.onStop();
     }
 
     @Override
     public void onDestroy(){
-        Log.i("LifeCycleSample","Main onDestroy() called.");
+        Log.i("LifeCycleSample","Sub onDestroy() called.");
         super.onDestroy();
     }
 
     public void onButtonClick(View view){
-        Intent intent = new Intent(LifeCycleMainActivity.this,LifeCycleMainActivity.class);
-        startActivity(intent);
+        finish();
+        }
     }
-}
